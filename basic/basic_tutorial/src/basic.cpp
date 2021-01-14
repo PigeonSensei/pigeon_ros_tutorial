4d4781bc784cb00f161b88356b54799cc487296f
PigeonSensei <parkour5813@gmail.com>
2021-01-14 11:03:04 +0900

    -

---
+++ basic/basic_tutorial/src/basic.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "basic_node");
+  ros::NodeHandle n;
+
+  ROS_INFO("Hello world!");
+}
---
+++ class/basic_class_tutorial/include/basic_class.h
@@ -0,0 +1,31 @@
+#ifndef BASIC_CLASS_H
+#define BASIC_CLASS_H
+
+#include <ros/ros.h>
+
+#include <termios.h>
+#include <unistd.h>
+
+class Basic_class
+{
+public:
+    Basic_class(ros::NodeHandle &n)
+       {
+          // open run
+          ROS_INFO("basic_class_node Open");
+       }
+       ~Basic_class()
+       {
+          // close run
+          ROS_INFO("basic_class_node Close");
+       }
+
+    int ReturnInputKey();
+
+    bool Update();
+
+private:
+
+};
+
+#endif // BASIC_CLASS_H
