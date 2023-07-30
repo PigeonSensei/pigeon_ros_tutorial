83ebdf7b3a5961fc869265844b493338100a9194
PigeonSensei <parkour5813@gmail.com>
2023-07-31 02:06:10 +0900

    update client node

--- basic/basic_service_client_tutorial/src/basic_service_client.cpp
+++ basic/basic_service_client_tutorial/src/basic_service_client.cpp
@@ -8,14 +8,16 @@ int main(int argc, char **argv)
 
   ROS_INFO("basic_service_client_node Open");
 
-  ros::ServiceClient service_client_tutorial_command = n.serviceClient<tutorial_srvs::TutorialSrv>("tutorial_command");
+  ros::ServiceClient service_client_tutorial_command;
+  service_client_tutorial_command = n.serviceClient<tutorial_srvs::TutorialSrv>("tutorial_command");
+
   tutorial_srvs::TutorialSrv tutorial_command;
 
   tutorial_command.request.command = "tutorial 1";
 
-  service_client_tutorial_command.call((tutorial_command));
+  service_client_tutorial_command.call(tutorial_command);
 
-  ROS_INFO("rosservice call /tutorial_command command: '%s'", tutorial_command.request.command.c_str());
+  ROS_INFO("rosservice call /tutorial_command command : '%s'", tutorial_command.request.command.c_str());
 
   ROS_INFO("Service Call Response result : %s", tutorial_command.response.result.c_str());
 
