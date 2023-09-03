3190d352b8433195f3154572fa7848c37061ac96
PigeonSensei <parkour5813@gmail.com>
2023-09-03 09:25:05 +0900

    loop_late->loop_rate

--- basic/basic_tf_tutorial/src/basic_tf.cpp
+++ basic/basic_tf_tutorial/src/basic_tf.cpp
@@ -9,18 +9,18 @@ int main(int argc, char **argv)
 
   ROS_INFO("basic_tf_node Open");
 
-  ros::Rate loop_late(60);
+  ros::Rate loop_rate(60);
 
   tf::TransformBroadcaster broadcaster;
 
   while(ros::ok())
   {
     broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0.0, 0.0, 0.0, 1.0), tf::Vector3(1.0, 0.0, 0.0)),
-                                                      ros::Time::now(),
-                                                      "parent_link",
-                                                      "child_link"));
+                                                   ros::Time::now(),
+                                                   "parent_link",
+                                                   "child_link"));
     ros::spinOnce();
-    loop_late.sleep();
+    loop_rate.sleep();
 
   }
 
